03e05364ecd1e177c1ec8deac8f27dd98b41fefd
ZerlsDev <62802943+ZerlsDev@users.noreply.github.com>
2021-01-20 16:48:27 +0000

    Create index.js

---
+++ src/functions/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    getUserStats: require('./getUserStats'),
+    getUserQueue: require('./getUserQueue'),
+    inGame: require('./inGame'),
+    getMatchByGameId: require('./getMatchByGameId'),
+    getMatchByUserId: require('./getMatchByUserId'),
+    getTeamByTeamId: require('./getTeamByTeamId'),
+    getTeamByUserId : require('./getTeamByUserId ')
+};
