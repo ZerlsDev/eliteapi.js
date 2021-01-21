64c1eb9a061b204808c9d66b52cab244f2d39147
ZerlsDev <62802943+ZerlsDev@users.noreply.github.com>
2021-01-21 18:41:14 +0000

    Update get.js

--- src/functions/get.js
+++ src/functions/get.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch');
+const config = require('../config.js');
 
 module.exports = async function (id, authorization, endpoint) {
-    return fetch(`https://${config.domain}${config.endpoints[endpoint].replace(':id', `${id}`)}`, {
+    return fetch(`https://${config.domain}${endpoint.replace(':id', `${id}`)}`, {
         headers: {
             'Content-Type': 'application/json',
             'authorization': authorization
